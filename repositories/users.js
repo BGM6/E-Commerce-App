a8d1f34d9a07f3e99321b86b291215bee5432dc4
bguilas <bryanjguilas@gmail.com>
2020-12-12 18:46:10 -0800

    finish users.js

--- repositories/users.js
+++ repositories/users.js
@@ -39,9 +39,10 @@ class UsersRepository {
     }
 
     async getOne(id) {
-    //find the given id in the user.json file;
+        //find the given id in the user.json file;
         const records = await this.getAll();
         return records.find(record => record.id === id);
+
     }
 
     async delete(id) {
@@ -54,14 +55,37 @@ class UsersRepository {
         const records = await this.getAll();
         const record = records.find(record => record.id === id);
 
+        if (!record) {
+            throw new Error(`Record with id ${id} not found`);
+        }
+
+        Object.assign(record, attrs);
+        await this.writeAll(records);
     }
 
+    async getOneBy(filters) {
+        const records = await this.getAll();
+
+        for (let record of records) {
+            let found = true;
+
+            for (let key in filters) {
+                if (record[key] !== filters[key]) {
+                    found = false;
+                }
+            }
+            if (found) {
+                return record;
+            }
+        }
+    }
 }
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
-    await repo.delete('23da948b');
+    const user = await repo.getOneBy({email: 'test@test.com'});
 
+    console.log(user)
 };
 
 test();
