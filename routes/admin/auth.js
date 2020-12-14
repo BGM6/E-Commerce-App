f38c2e56995b67dfd2634682a7fbe510f8305c7a
bguilas <bryanjguilas@gmail.com>
2020-12-13 20:15:40 -0800

    addex express validation

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const usersRepo = require('../../repositories/users.js');
+const {check, validationResult} = require('express-validator');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
 
@@ -33,26 +34,78 @@ router.get('/signin', (req, res) => {
     res.send(signinTemplate());
 });
 
+router.post(
+    '/signup',
+    [
+        check('email')
+            .trim()
+            .normalizeEmail()
+            .isEmail()
+            .withMessage('Must be a valid email')
+            .custom(async (email) => {
+                const existingUser = await usersRepo.getOneBy({email});
+                if (existingUser) {
+                    throw new Error('Email in use');
+                }
+            }),
+        check('password')
+            .trim()
+            .isLength({min: 4, max: 20}),
+        check('passwordConfirmation')
+            .trim()
+            .isLength({min: 4, max: 20})
+            .withMessage('Must be between 4 and 20 characters')
+            .custom((passwordConfirmation, {req}) => {
+                if (passwordConfirmation !== req.body.password) {
+                    throw new Error('Password must match');
+                }
+            })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        console.log(errors);
+
+        const {email, password, passwordConfirmation} = req.body;
+
+        // Create a user in our user repo to represent this person
+        const user = await usersRepo.create({email, password});
+
+        // Store the id of that user inside the users cookie
+        req.session.userId = user.id;
+
+        res.send('Account created!!!');
+    }
+)
+;
+
+router.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are logged out');
+});
+
+router.get('/signin', (req, res) => {
+    res.send(signinTemplate());
+});
+
 router.post('/signin', async (req, res) => {
     const {email, password} = req.body;
-    const user = await usersRepo.getOneBy({email: email});
+    const user = await usersRepo.getOneBy({email});
+
     if (!user) {
         return res.send('Email not found');
     }
 
-    const validPassword = await usersRepo.comparePassword(
+    const validPassword = await usersRepo.comparePasswords(
         user.password,
         password
-    )
-
+    );
     if (!validPassword) {
         return res.send('Invalid password');
     }
 
-    req.session.userId = user.id; //This is the line that logs in the user after validation is passed
+    req.session.userId = user.id;
 
-    res.send('You are signed in');
+    res.send('You are signed in!!!');
 });
-
 
 module.exports = router;
