d31b55987a2ec9576e4bcb46676d2cb161d3456b
bguilas <bryanjguilas@gmail.com>
2020-12-16 20:25:34 -0800

    added validation to edit

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -49,7 +49,6 @@ router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
         return res.send('Product not found');
 
     }
-
     res.send(productsEditTemplate({product: product}));
 
 });
@@ -58,7 +57,10 @@ router.post('/admin/products/:id/edit',
     requireAuth,
     upload.single('image'),
     [requireTitle, requirePrice],
-    handleErrors(productsEditTemplate),
+    handleErrors(productsEditTemplate, async (req) => {
+        const product = await productsRepo.getOne(req.params.id);
+        return { product: product };
+    }),
     async (req, res) => {
         const changes = req.body;
         if (req.file) {
